c6786b9f862ef42764543a8bb8b07ef9792c4623
Rich Boakes <rjb@port.ac.uk>
2022-11-21 17:39:30 +0000

    fix times for agenda component

--- src/js/lib/icalparse.js
+++ src/js/lib/icalparse.js
@@ -95,7 +95,7 @@ function getEventInfo(strEvent, timeZone, email) {
   const startDateNumber = new Date(utcDate);
   if (startDateNumber < new Date(Date.now())) { return null; }
   result.startDate = startDateNumber.toLocaleString('en-GB', { timeZone });
-  result.title = getDateString(startDateNumber) + result.title;
+//  result.title = getDateString(startDateNumber) + result.title;
   year = parseInt(dtEnd.substr(0, 4));
   month = parseInt(dtEnd.substr(4, 2)) - 1;
   day = parseInt(dtEnd.substr(6, 2));
