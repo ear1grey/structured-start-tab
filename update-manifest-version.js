4385b82f472b76d8a9d2ebe658ca29bbb5f89f8e
adrianoBP <adriano.boccardo@gmail.com>
2022-12-01 19:12:38 +0000

    feat: add beta indication

--- update-manifest-version.js
+++ update-manifest-version.js
@@ -9,4 +9,14 @@ const pkgData = JSON.parse(pkgRaw);
 const manRaw = readFileSync(manPath, 'utf8');
 const manData = JSON.parse(manRaw);
 manData.version = pkgData.version;
+
+pkgData.name = 'structured-start-tab';
+pkgData.description = 'Structured Start Tab';
+
+if (pkgData.version.includes('beta')) {
+  pkgData.name += '-beta';
+  pkgData.description += ' (beta)';
+}
+
 writeFileSync(manPath, JSON.stringify(manData, null, 2));
+writeFileSync(pkgPath, JSON.stringify(pkgData, null, 2));
